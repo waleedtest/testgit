453401db9f1c2deb2b7968bff7b8e1ec4da922ff
waleedtest <70428145+waleedtest@users.noreply.github.com>
2020-08-29 16:21:51 +0800

    Update main.c
    add branch:customer_1 and test

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void customer_1(void)
+{
+  printf("This is customer_1 branch!");
+}
+
 void main(void)
 {
+  customer_1();
   printf("This is main code!");
 }
